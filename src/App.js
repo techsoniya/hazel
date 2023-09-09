55ed68fa8ac9d7608a52a82762d822e25c54dae4
Soniya Kambli <kamblisoniya@gmail.com>
2023-09-09 15:06:01 +0530

    modified:   src/App.css modified:   src/App.js deleted:    src/App.test.js new file:   src/components/RecipeCard.jsx new file:   src/components/SearchBar.jsx deleted:    src/index.css modified:   src/index.js deleted:    src/logo.svg deleted:    src/reportWebVitals.js deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchBar from "./components/SearchBar";
+import RecipeCard from"./components/RecipeCard";
+const apiUrl ="https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] =  useState(false);
+  const [query,setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  //function to search for the recipes 
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = apiUrl + query
+    const res = await fetch(url);
+    const data =await res.json();
+    //console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(()=>{
+    searchRecipes()
+  },[]);
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    searchRecipes()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Our Recipe App</h2>
+      <SearchBar 
+           isLoading={isLoading}
+           query={query}
+           setQuery={setQuery}
+           handleSubmit={handleSubmit}
+           
+      />
+      <div className="recipes">
+        {recipes ? recipes.map(recipe=> (
+          <RecipeCard 
+            key={recipe.idMeal} 
+            recipe={recipe} 
+          />
+        )) : "No recipes"}
+      </div>
+      
     </div>
   );
 }
